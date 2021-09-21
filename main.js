af6fb662fdd4ade971e8977c2bd0a18f644f0a32
robertdbjohnson <joh16151@byui.edu>
2021-09-21 16:41:48 -0600

    Rough start to index.html and main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// const links = [
+//     {
+//         label: "Week1 notes",
+//         url: "week1/index.html"
+//     },
+//     {
+//         label: "Week2 notes",
+//         url: "week2/index.html"
+//     }
+// ];
+
+// var ToCList = document.getElementById("ToC").innerHTML;
+// var listLength = links.length;
+
+// for (let i = 0; i < listLength; i++){
+
+// }
